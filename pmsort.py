fad25ce1185578055ff7fd2f2445d77aa1a4e94c
Ilia Glazkov <ilia.glazkov@gmail.com>
2011-10-01 13:59:43 +0400

    Single-process merge sort

--- pmsort.py
+++ pmsort.py
@@ -2,49 +2,63 @@
 
 import struct
 import numpy
-import array
+#import array
+import os
+from collections import deque
+from heapq import merge
+import shutil
+
+import tempfile
 
 READ_BUF = 8192
 INPUT_FILE = 'input'
 OUTPUT_FILE = 'output'
+SORT_MEMORY_COUNT = 1000000
+
+def sort_in_memory(f, count):
+    data = numpy.fromfile(file=f, dtype=numpy.uint32, count=count)
+    data.sort()
+    return data
+#    with open(OUTPUT_FILE, 'wb') as fout:
+#        data.tofile(fout)
+
+def read_file(f):
+    for chunk in iter(lambda: f.read(READ_BUF), ''):
+        for c in xrange(len(chunk)/4):
+            yield struct.unpack('I', chunk[c*4:c*4+4])[0]
+
+def write_file(f, iterable):
+    for item in iterable:
+        f.write(struct.pack('I', item))
+
+
+def merge_files(tmpdir, files):
+    while len(files) > 1:
+        print len(files)
+        with open(files.popleft(), 'rb') as f1, open(files.popleft(), 'rb') as f2:
+            fd, pathname = tempfile.mkstemp(dir=tmpdir)
+            with os.fdopen(fd, 'wb') as fout:
+                write_file(fout, merge(read_file(f1), read_file(f2)))
+            files.append(pathname)
+    return files.pop()
 
 def main():
-    fin = open(INPUT_FILE, 'rb')
-    for chunk in fin.read(READ_BUF):
-        pass
-
-def main_np():
+    tmpdir = tempfile.mkdtemp()
+    files = deque()
+    # In memory stage
     with open(INPUT_FILE, 'rb') as fin:
-        data = numpy.fromfile(file=fin, dtype=numpy.uint32)
-    data.sort()
-    with open(OUTPUT_FILE, 'wb') as fout:
-        data.tofile(fout)
-
-def main_array():
-    data = array.array('I')
-    with open(INPUT_FILE, 'rb') as fin:
-        try: data.fromfile(fin, 4194304)
-        except EOFError: pass
-    data = array.array('I', sorted(data))
-    with open(OUTPUT_FILE, 'wb') as fout:
-        data.tofile(fout)
-
-def main_via_struct():
-    fin = open(INPUT_FILE, 'rb')
-    data = []
-    for chunk in iter(lambda: fin.read(4), ''):#READ_BUF):
-        data.append(struct.unpack('I', chunk)[0])
-    with open(OUTPUT_FILE, 'wb') as fout:
-        data.sort()
-        for item in data:
-            fout.write(struct.pack('I', item))
+        while True:
+            data = sort_in_memory(fin, SORT_MEMORY_COUNT)
+            if not len(data):
+                break
+            fd, pathname = tempfile.mkstemp(dir=tmpdir)
+            with os.fdopen(fd, 'wb') as fout:
+                data.tofile(fout)
+            files.append(pathname)
+    # Merge stage
+    final = merge_files(tmpdir, files)
+    shutil.move(final, OUTPUT_FILE)
+    shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
-    from timeit import Timer
-    t1 = Timer("main_np()", "from __main__ import main_np")
-    t2 = Timer("main_array()", "from __main__ import main_array")
-    t3 = Timer("main_via_struct()", "from __main__ import main_via_struct")
-    n = 10
-    #print t3.timeit(number=n)
-    print t1.timeit(number=n)
-    #print t2.timeit(number=n)
+    main()
